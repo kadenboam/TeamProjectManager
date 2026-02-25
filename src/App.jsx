86520189116759206dee20f18180cb6df1826e46
kadenboam <boa24015@byui.edu>
2026-02-25 14:18:40 -0700

    fixed_page_linking

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './App.css'
 import { useState, useEffect } from 'react';
 import supabase from "./config/supabaseClient"
@@ -69,6 +69,8 @@ function App() {
 
   return (
     <>
+      <Link to="/page2">Go to Page 2</Link>
+      <Link to="/projects">Go to other page</Link>
       <div>
         <h1>Team Project Helper</h1>
       </div>
--- src/ProjectTaskManager.jsx
+++ src/ProjectTaskManager.jsx
@@ -1,7 +1,7 @@
 // ═══════════════════════════════════════════════════════════════════════════════
 // THREE DYNAMIC DATASETS
 // ═══════════════════════════════════════════════════════════════════════════════
-
+import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import supabase from './config/supabaseClient'
 
